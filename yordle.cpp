7b506a726ec7545e3a8fc334cef0c0abeaa0fc50
Uma Kothuri <uma@kothuri.net>
2022-02-11 22:47:17 -0600

    Added highlighted keyboard after each turn

--- yordle.cpp
+++ yordle.cpp
@@ -4,7 +4,12 @@
 #include <time.h>
 using namespace std;
 
-enum Color {red, yellow, green};
+enum Color {red, yellow, green, grey};
+
+struct key {
+    char letter;
+    enum Color code;
+};
 
 void evaluateGuess(string answer, string guess, enum Color code[]){
     //set code to all red
@@ -46,14 +51,67 @@ bool isValid(string guess) {
     return false;
 }
 
+void updateKeyboard(key myKeys[], string guess, Color myCode[]){
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 26; j++){
+            if(guess[i] == myKeys[j].letter && myKeys[j].code != green){
+                if(myKeys[j].code == grey || ((myKeys[j].code == yellow) && myCode[i] == green))
+                    myKeys[j].code = myCode[i];
+            }
+        }
+    }
+}
+void printKeyboard(key myKeys[]){
+    printf("--------------------\n");
+    for(int i = 0; i < 10; i++){
+        if(myKeys[i].code == red)
+            printf("\033[;31m%c \033[0m", myKeys[i].letter) ;
+        else if(myKeys[i].code == yellow)
+            printf("\033[;33m%c \033[0m", myKeys[i].letter);
+        else if(myKeys[i].code == green)
+            printf("\033[;32m%c \033[0m", myKeys[i].letter);
+        else
+            printf("%c ", myKeys[i].letter);
+    }
+
+    cout << endl;
+
+    for(int i = 10; i < 19; i++){
+        if(myKeys[i].code == red)
+            printf("\033[;31m%c \033[0m", myKeys[i].letter) ;
+        else if(myKeys[i].code == yellow)
+            printf("\033[;33m%c \033[0m", myKeys[i].letter);
+        else if(myKeys[i].code == green)
+            printf("\033[;32m%c \033[0m", myKeys[i].letter);
+        else
+            printf("%c ", myKeys[i].letter);
+    }
+
+    cout << endl;
+
+    for(int i = 19; i < 26; i++){
+        if(myKeys[i].code == red)
+            printf("\033[;31m%c \033[0m", myKeys[i].letter) ;
+        else if(myKeys[i].code == yellow)
+            printf("\033[;33m%c \033[0m", myKeys[i].letter);
+        else if(myKeys[i].code == green)
+            printf("\033[;32m%c \033[0m", myKeys[i].letter);
+        else
+            printf("%c ", myKeys[i].letter);
+    }
+    printf("\n--------------------");
+
+}
+
 string welcomeMessage = "\nWelcome to Yordle! Guess the 5-Letter Word.\n\n\033[;31mRed\033[0m indicates a letter that is not present in the word.\n\033[;33mYellow\033[0m indicates a letter that is present elsewhere in the word.\n\033[;32mGreen\033[0m indicates a correctly placed letter.\n";
 
 int main() {
     Color myCode[5];
-
+    key myKeys[26];
+    string alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";    
     string guess;
     bool success = false;
-    int guess_count = 0;
+    int guess_count = 6;
 
     //welcome message
     cout << endl << "-------------------------------------------" << endl;
@@ -71,15 +129,21 @@ int main() {
     }
 
     MyReadFile.close();
-    
+
     for(int i = 0; i < 5; i++)
         answer.at(i) = toupper(answer.at(i));
-    
-    //playing game
-    while(!success && guess_count < 6){
+
+    //initialize keyboard
+    for (int i = 0; i < 26; i++){
+        myKeys[i].letter = alphabet[i];
+        myKeys[i].code = grey;
+    }
+
+    //game
+    while(!success && guess_count > 0){
 
         //obtain valid guess
-        cout << "Please enter your guess: ";
+        cout << "You have " << guess_count << " guesses left.\nPlease enter your guess: ";
         cin >> guess; cout << endl << endl;
 
         while(!isValid(guess)){
@@ -94,7 +158,7 @@ int main() {
 
         //evaluate guess and print encoded guess
         evaluateGuess(answer, guess, myCode);
-        
+
         for(int i = 0; i < 5; i++){
             if(myCode[i] == red)
                 printf("\033[;31m%c\033[0m ", guess.at(i));
@@ -105,10 +169,15 @@ int main() {
         }
         cout << endl << endl;
 
+        updateKeyboard(myKeys, guess, myCode);
+        printKeyboard(myKeys);
+
+        cout << endl << endl;
+
         //finish turn
         if(guess == answer)
             success = true;
-        guess_count++;
+        guess_count--;
     }
 
     //end of game
